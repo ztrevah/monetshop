5d504fa7a204a45d9c9e59ba5686b99f14a7048e
ztrevah <chien03052003@gmail.com>
2024-06-29 17:21:20 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors({credentials: true}));
+app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth/",authRoutes);
@@ -24,11 +24,6 @@ app.use("/api/management/",managementRoutes);
 let PORT = process.env.PORT || 9090;
 app.listen(9090,() => {
     console.log("Connected");
-});
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
 });
 app.get("/",(req,res) => {
     return res.status(200).json("Connected");
